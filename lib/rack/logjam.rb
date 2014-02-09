74736b5400d95b264277fb7c8bc8f25455c3078f
Nils Jonsson <nils@alumni.rice.edu>
2014-02-09 00:04:34 -0600

    Add missing xterm colors logic

--- lib/rack/logjam.rb
+++ lib/rack/logjam.rb
@@ -3,6 +3,7 @@ module Rack
   module Logjam
 
     autoload :VERSION,    'rack/logjam/version'
+    autoload :ANSI,       'rack/logjam/ansi'
     autoload :Middleware, 'rack/logjam/middleware'
 
   end
